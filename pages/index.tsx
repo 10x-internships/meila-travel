3466b97f2d30cb121643b389391a0ba0f8fc1054
Nguyên Trần <nguyentranthepog@gmail.com>
2021-07-08 14:11:38 +0700

    feat: add Layout to _app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/Home.module.css';
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Meila Travel</title>
         <meta name="description" content="Explore the world with Meila Travel" />
       </Head>
-    </div>
+    </>
   );
 }
