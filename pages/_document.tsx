955c026c65c805a97adf966c57535f5814b18e0c
Nguyên Trần <nguyentranthepog@gmail.com>
2021-07-13 16:01:20 +0700

    feat: add Gallery section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&family=Josefin+Sans:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
